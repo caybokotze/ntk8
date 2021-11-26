38af333d58d9e5f58d3fcba46bb42f6657781310
Caybo Kotzé <kotzecabo@gmail.com>
2021-11-26 14:12:12 +0200

    :sparkles: change responses to long on the user DTOs

--- Ntk8/Dto/AuthenticatedResponse.cs
+++ Ntk8/Dto/AuthenticatedResponse.cs
@@ -4,7 +4,7 @@ namespace Ntk8.Dto
 {
     public class AuthenticatedResponse
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Title { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
